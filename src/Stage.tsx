6b23e7396b1c52b682ed870e0e3e102ab8823297
Lord-Raven <93345679+Lord-Raven@users.noreply.github.com>
2024-05-31 22:29:59 +0000

    Moving to text choices.

--- src/Stage.tsx
+++ src/Stage.tsx
@@ -52,12 +52,12 @@ type ChatStateType = any;
 export class Stage extends StageBase<InitStateType, ChatStateType, MessageStateType, ConfigType> {
 
     readonly defaultStat: number = 0;
-    readonly actionPrompt: string = 'Develop an excerpt of organic narration. At the end of the message, append three or four varied follow-up stat-oriented action suggestions that {{user}} could choose to take, formatted as such:\n' +
+    readonly actionPrompt: string = 'Develop an excerpt of organic narration. At the end of this passage, divise and append three or four varied, stat-oriented, active follow-up suggestions that the user could choose to take, formatted as such:\n' +
         '"(Stat +Modifier) Brief summary of action"\n' +
         '"Stat" is one of these eight core stats:\n' +
         Object.keys(Stat).map(key => `${key}: ${StatDescription[key as Stat]}`).join('\n') +
         'And "Modifier" is a relative difficulty modifier between -5 and 5 which will be added to the skill check result; a lower number reflects a more difficult task.\n' +
-        'Place each option on a separate line and study the stat descriptions for inspiration. Here are sample options:\n' +
+        'Place each option on a separate line. Study the stat descriptions for inspiration and consider the characters\' current situations and assets. Here are sample options:\n' +
         '"(Might +1) Force the lock"\n' +
         '"(Skill -1) Pick the lock (it looks difficult)"\n' +
         '"(Grace +0) Scale the wall"\n' +
@@ -339,7 +339,7 @@ export class Stage extends StageBase<InitStateType, ChatStateType, MessageStateT
         this.lastOutcome = outcome;
         this.lastOutcomePrompt = '';
         if (this.lastOutcome) {
-            this.lastOutcomePrompt += '{{user}} has chosen the following action: ' + this.lastOutcome.action.description; + '\n';
+            this.lastOutcomePrompt += `The user has chosen the following action: ${this.lastOutcome.action.description}\n`;
             this.lastOutcomePrompt += `${ResultDescription[this.lastOutcome.result]}\n`
         }
     }
