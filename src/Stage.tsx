0d544d2b56a32f5ad4d695c1ff9bd6341a030fd4
Lord-Raven <93345679+Lord-Raven@users.noreply.github.com>
2024-08-05 16:47:57 -0400

    Code cleanup. Return to the preceding commit, if I ever return to CYOA style options.

--- src/Stage.tsx
+++ src/Stage.tsx
@@ -1,8 +1,8 @@
 import {ReactElement} from "react";
-import {StageBase, StageResponse, InitialData, Message, TextGenRequest, Character, User} from "@chub-ai/stages-ts";
+import {StageBase, StageResponse, InitialData, Message, Character, User} from "@chub-ai/stages-ts";
 import {LoadResponse} from "@chub-ai/stages-ts/dist/types/load";
 import {Action} from "./Action";
-import {Stat, StatDescription} from "./Stat"
+import {Stat} from "./Stat"
 import {Outcome, Result, ResultDescription} from "./Outcome";
 import {env, pipeline} from '@xenova/transformers';
 
@@ -23,66 +23,26 @@ type ChatStateType = any;
 export class Stage extends StageBase<InitStateType, ChatStateType, MessageStateType, ConfigType> {
     
     readonly defaultStat: number = 0;
-    readonly adLibPrompt: string = 'Determine whether the preceding input includes stat-based action or motivated dialog. \n' +
-        'If so, merely output the name of the stat that best governs the action or intent, with a relative difficulty modifier between -5 and +5 (higher numbers representing lower risk).\n' +
-        'If not, simply output "None".\n' +
-        'These are the eight possible stats and their descriptions, to aid in selecting the most applicable:\n' +
-        Object.keys(Stat).map(key => `${key}: ${StatDescription[key as Stat]}`).join('\n') + '\n' +
-        'Sample responses:\n"Might +1"\n"Skill -2"\n"Grace +0"\n"None"';
-    readonly actionPrompt: string = 
-        '[INST]Based on the above chat history, output a list of three-to-six options for varied follow-up actions that {{user}} could choose to pursue next.\n' +
-        'These options can be simple dialog, immediate reactions, or generic courses of action. ' +
-        'If the option involves any risk, an associated stat and difficulty modifier is included. ' +
-        'All options follow this format:\n' +
-        '-(Stat +Modifier) Brief summary of action\n' +
-        'These are all eight possible stats with a brief description and example verb associations:\n' +
-        Object.keys(Stat).map(key => `${key}: ${StatDescription[key as Stat]}`).join('\n') +
-        'The modifier is a relative difficulty adjustment between -5 and +5 which will be added to the skill check result; give easy tasks a higher number and riskier options a negative number.\n' +
-        'Output each option on a separate line. Study the stat descriptions for inspiration and consider the characters\' current situations, motivations, and assets.\n' +
-        '[SAMPLE]\n' +
-        '-Talk to the guard about admittance.\n' +
-        '-(Charm -2) Convince the guard to let you in.\n' +
-        '-(Might +1) Force the lock.\n' +
-        '-(Skill -1) Pick the lock (it looks difficult).\n' +
-        '-(Luck -1) Search for another way in.\n' +
-        '-Give up.\n[/SAMPLE]\n' +
-        'Although the flavor of the options should exercise creativity, the presentation of these options should be uniform and plain at all times.[/INST]';
-
-    // Regular expression to match the pattern "(Stat +modifier) description"
-    readonly actionRegex = /(\w+)\s*([-+]\d+)\s*[^a-zA-Z]+\s*(.*)/; // /(\w+)\s*([-+]\d+)\s*[^a-zA-Z]+\s*(.+)/
-    readonly whitespaceRegex = /^[\s\r\n]*$/;
-    readonly nonLetterRegex = /^[^a-zA-Z]+/;
-
     readonly levelThresholds: number[] = [2, 5, 8, 12, 16, 20, 25, 30, 35, 40, 45, 50, 55, 60, 65, 70, 75, 80, 85, 90, 95, 100];
-    
-    currentMessage: string = '';
-    actions: Action[] = [];
-    currentMessageId: string|undefined = undefined;
-    lastOutcome: Outcome|null = null;
-    lastOutcomePrompt: string = '';
-    promptForId: string|undefined = undefined;
-    player: User;
-    character: Character;
-    
+
     // message-level variables
     experience: number = 0;
-    messageHistory: string = '';
     statUses: {[stat in Stat]: number} = this.clearStatMap();
     stats: {[stat in Stat]: number} = this.clearStatMap();
+    lastOutcome: Outcome|null = null;
+    lastOutcomePrompt: string = '';
 
     // other
     zeroShotPipeline: any;
+    player: User;
+    character: Character;
 
     constructor(data: InitialData<InitStateType, ChatStateType, MessageStateType, ConfigType>) {
         super(data);
         const {
             characters,
             users,
-            config,
             messageState,
-            environment,
-            initState,
-            chatState
         } = data;
         this.setStateFromMessageState(messageState);
         this.player = users[Object.keys(users)[0]];
@@ -125,56 +85,16 @@ export class Stage extends StageBase<InitStateType, ChatStateType, MessageStateT
         this.setStateFromMessageState(state);
     }
 
-    async addMessageToHistory(message: string, prefix?: string) {
-        const threshold = 2000;
-        const responsePrefix = prefix ?? '###Response: ';
-        if (message.length > 0) {
-            this.messageHistory += responsePrefix + message;
-        }
-        while (this.messageHistory.length > threshold) {
-            let responseIndex = this.messageHistory.indexOf(responsePrefix, 1);
-            if (responseIndex === -1) {
-                return;
-            }
-            this.messageHistory = this.messageHistory.substring(responseIndex);
-        }
-    }
-
     async beforePrompt(userMessage: Message): Promise<Partial<StageResponse<ChatStateType, MessageStateType>>> {
         const {
-            content,
-            anonymizedId,
-            isBot,
-            promptForId,
-            identity
+            content
         } = userMessage;
-
-        this.promptForId = promptForId ?? undefined;
-        this.currentMessageId = identity;
 
         let errorMessage: string|null = null;
         let takenAction: Action|null = null;
         let finalContent: string|undefined = content;
 
-        // Attempt to parse actions:
-        for (let i = 0; i < this.actions.length; i++) {
-            const action: Action = this.actions[i];
-            if (action.stat && content.toLowerCase().includes(action.stat.toLowerCase())) {
-                console.log('Chose action by stat');
-                takenAction = action;
-                break;
-            } else if (content === `${i + 1}` || content === `${i + 1}.`) {
-                console.log('Chose action by number');
-                takenAction = action;
-                break;
-            } else if (content.length > 6 && action.description.toLowerCase().includes(content.toLowerCase())) {
-                console.log('Chose action by description');
-                takenAction = action;
-                break;
-            }
-        }
-
-        if (!takenAction && finalContent && this.zeroShotPipeline != null) {
+        if (finalContent && this.zeroShotPipeline != null) {
             const statMapping:{[key: string]: string} = {'Muscle and Endurance': 'Might', 'Agility and Composure': 'Grace', 'Craft and Deftness': 'Skill', 'Logic and Knowledge': 'Brains', 'Instinct and Awareness': 'Wits', 'Allure and Influence': 'Charm', 'Empathy and Character': 'Heart', 'Karma and Luck': 'Luck'};
             let topStat: Stat|null = null;
             this.zeroShotPipeline.task = 'Choose a set of attributes that best describe or govern the actions taken in this narrative passage.'
@@ -192,23 +112,6 @@ export class Stage extends StageBase<InitStateType, ChatStateType, MessageStateT
             if (difficultyResponse && difficultyResponse.labels[0]) {
                 difficultyRating = difficultyMapping[difficultyResponse.labels[0]];
             }
-
-
-
-            /*let textGenRequest: TextGenRequest = {
-                prompt: `${finalContent}\n[${this.adLibPrompt}]`,
-                max_tokens: 100,
-                min_tokens: 50,
-                stop: [],
-                include_history: false,
-                template: '',
-                context_length: 500
-            };
-            let textResponse = await this.generator.textGen(textGenRequest);
-            const adLibPattern = new RegExp(`^(${Object.values(Stat).join('|')}) ((\\+|-)\\d+)`);
-            console.log('request complete?');
-            console.log(textResponse);
-            const match = adLibPattern.exec(textResponse?.result ?? '');*/
 
             if (topStat) {
                 takenAction = new Action(finalContent, topStat, difficultyRating, this.stats[topStat]);
@@ -245,8 +148,6 @@ export class Stage extends StageBase<InitStateType, ChatStateType, MessageStateT
             }
         }
 
-        await this.addMessageToHistory(this.lastOutcomePrompt, '###Input: ');
-
         return {
             stageDirections: `\n[INST]${this.lastOutcomePrompt}\n[/INST]`,
             messageState: this.buildMessageState(),
@@ -261,72 +162,17 @@ export class Stage extends StageBase<InitStateType, ChatStateType, MessageStateT
         return Object.values(this.stats).reduce((acc, val) => acc + val, 0)
     }
     async afterResponse(botMessage: Message): Promise<Partial<StageResponse<ChatStateType, MessageStateType>>> {
-        const {
-            content,
-            anonymizedId,
-            isBot,
-            identity
-        } = botMessage;
 
         this.lastOutcomePrompt = '';
-
-        await this.addMessageToHistory(content);
-        
-        // Generate options:
-        /*let optionPrompt = this.replaceTags(`[{{char}} DESCRIPTION]\n${this.character.description} ${this.character.personality}[/{{char}} DESCRIPTION]\n[{{user}} DESCRIPTION]\n${this.player.chatProfile}[/{{user}} DESCRIPTION]\n[HISTORY]\n${this.messageHistory}\n[/HISTORY]\n${this.actionPrompt}`,
-            {"user": this.player.name, "char": this.character.name, "original": ''});
-        let optionResponse = await this.generator.textGen({
-            prompt: optionPrompt,
-            max_tokens: 150
-        });
-
-        let tmpActions:Action[] = [];
-        
-        if (optionResponse && optionResponse.result) {
-            console.log(`Option response`);
-            console.log(optionResponse.result);
-            const lines = optionResponse.result.split('\n');
-            const regex = /^[-*]/;
-
-            for (const line of lines) {
-                const match = line.match(this.actionRegex);
-                if (match && !match[3].match(this.whitespaceRegex) && match[1] in Stat) {
-                    console.log('Have an action: ' + match[3] + ';' + match[1] + ';' + match[2]);
-                    tmpActions.push(new Action(match[3], match[1] as Stat, Number(match[2])));
-                } else if (regex.test(line.trim()) && !line.replace(this.nonLetterRegex, "").match(this.whitespaceRegex)) {
-                    console.log('Have a stat-less action: ' + line.replace(this.nonLetterRegex, ""));
-                    tmpActions.push(new Action(line.replace(this.nonLetterRegex, ""), null, 0));
-                }
-            }
-        }
-
-        // Trim down options:
-        const uniqueStats = new Set<Stat>();
-        this.actions = tmpActions.filter(action => {
-            if (action.description.trim() == '') {
-                return false;
-            }
-            if (action.stat && !uniqueStats.has(action.stat)) {
-                uniqueStats.add(action.stat);
-                return true;
-            }
-            return !(action.stat);
-        });
-        this.actions.length = Math.min(this.actions.length, 6);*/
-
-        this.currentMessageId = identity;
-
-        //await this.addMessageToHistory(this.actions.length > 0 ? this.actions.map((action, index) => `${index + 1}. ${action.fullDescription()}`).join('\n') : '', '###Choice: ');
 
         return {
             stageDirections: null,
             messageState: this.buildMessageState(),
             modifiedMessage: null,
-            error: null, //this.actions.length == 0 ? 'Failed to generate actions; consider swiping or write your own.' : null,
+            error: null,
             systemMessage: `---\n` +
                 `\`{{user}} - Level ${this.getLevel() + 1} (${this.experience}/${this.levelThresholds[this.getLevel()]})\`<br>` +
                 `\`${Object.keys(Stat).map(key => `${key}: ${this.stats[key as Stat]}`).join(' | ')}\``,
-            // this.actions.length > 0 ? `Choose an action:\n` + this.actions.map((action, index) => `${index + 1}. ${action.fullDescription()}`).join('\n') : null,
             chatState: null
         };
     }
@@ -338,16 +184,9 @@ export class Stage extends StageBase<InitStateType, ChatStateType, MessageStateT
                 this.stats[stat as Stat] = messageState[stat] ?? this.defaultStat;
                 this.statUses[stat as Stat] = messageState[`use_${stat}`] ?? 0;
             }
-            this.currentMessage= messageState['currentMessage'] ?? '';
-            this.currentMessageId = messageState['currentMessageId'] ?? '';
             this.lastOutcome = messageState['lastOutcome'] ? this.convertOutcome(messageState['lastOutcome']) : null;
             this.lastOutcomePrompt = messageState['lastOutcomePrompt'] ?? '';
-            this.actions = messageState['actions'] ? messageState['actions'].map((action: any) => {
-                return this.convertAction(action);
-            }) : [];
-            this.promptForId = messageState['promptForId'];
             this.experience = messageState['experience'] ?? 0;
-            this.messageHistory = messageState['messageHistory'] ?? '';
         }
     }
 
@@ -365,14 +204,9 @@ export class Stage extends StageBase<InitStateType, ChatStateType, MessageStateT
             messageState[stat] = this.stats[stat as Stat] ?? this.defaultStat;
             messageState[`use_${stat}`] = this.statUses[stat as Stat] ?? 0;
         }
-        messageState['currentMessage'] = this.currentMessage ?? '';
-        messageState['currentMessageId'] = this.currentMessageId ?? '';
         messageState['lastOutcome'] = this.lastOutcome ?? null;
         messageState['lastOutcomePrompt'] = this.lastOutcomePrompt ?? '';
-        messageState['actions'] = this.actions ?? [];
-        messageState['promptForId'] = this.promptForId;
         messageState['experience'] = this.experience ?? 0;
-        messageState['messageHistory'] = this.messageHistory;
 
         return messageState;
     }
@@ -381,7 +215,7 @@ export class Stage extends StageBase<InitStateType, ChatStateType, MessageStateT
         this.lastOutcome = outcome;
         this.lastOutcomePrompt = '';
         if (this.lastOutcome) {
-            this.lastOutcomePrompt += `The user has chosen the following action: ${this.lastOutcome.action.description}\n`;
+            this.lastOutcomePrompt += `{{user}} has chosen the following action: ${this.lastOutcome.action.description}\n`;
             this.lastOutcomePrompt += `${ResultDescription[this.lastOutcome.result]}\n`
         }
     }
@@ -399,8 +233,6 @@ export class Stage extends StageBase<InitStateType, ChatStateType, MessageStateT
             display: 'grid',
             alignItems: 'stretch'
         }}>
-            <div>{this.currentMessage}</div>
-            <div>{this.actionPrompt}</div>
         </div>;
     }
 
