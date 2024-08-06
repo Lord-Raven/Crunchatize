0e97b452a6ccf9d5fb6faf5484dfc97d21739852
Lord-Raven <93345679+Lord-Raven@users.noreply.github.com>
2024-08-06 14:39:18 -0400

    Revisions to difficulty assessment and an attempt at detecting no-check situations.

--- src/Stage.tsx
+++ src/Stage.tsx
@@ -132,7 +132,7 @@ export class Stage extends StageBase<InitStateType, ChatStateType, MessageStateT
                 'Daunting, Arduous, Formidable, Demanding': -2,
                 'Impossible, Insurmountable': -3};
             let difficultyRating:number = 0;
-            this.zeroShotPipeline.task = 'Choose words that best reflect the effort depicted in this narrative passage.'
+            this.zeroShotPipeline.task = 'Choose the set of words that best reflect the effort this entails.'
             let difficultyResponse = await this.zeroShotPipeline(content, Object.keys(difficultyMapping), { multi_label: true });
             console.log(`Difficulty modifier selected: ${difficultyMapping[difficultyResponse.labels[0]]}`);
             console.log(difficultyResponse);
